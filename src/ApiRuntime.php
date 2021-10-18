44897e385d0b47d3ca18f2bab39f1e4ab47b98f9
julien <julien@kiboko.fr>
2021-10-18 11:28:24 +0200

    create api runtime

---
+++ src/ApiRuntime.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kiboko\Component\Runtime\Api;
+
+use Kiboko\Component\Runtime\Hook\HookRuntimeInterface;
+
+class ApiRuntime implements ApiRuntimeInterface
+{
+    private array $hookRuntimes = [];
+
+    public function addHook(string $hookName, HookRuntimeInterface $hookRuntime): self
+    {
+        $this->hookRuntimes[$hookName] = $hookRuntime;
+        return $this;
+    }
+
+    public function run(string $hookName): int
+    {
+        if (!array_key_exists($hookName, $this->hookRuntimes)) {
+            throw new \RuntimeException(
+                sprintf('Unknown hook runtime %s , please register it', $hookName)
+            );
+        }
+        return $this->hookRuntimes[$hookName]->run();
+   }
+
+    public function feed(string $hookName, ...$data): self
+    {
+        if (!array_key_exists($hookName, $this->hookRuntimes)) {
+            throw new \RuntimeException(
+                sprintf('Unknown hook runtime %s , please register it', $hookName)
+            );
+        }
+        $this->hookRuntimes[$hookName]->feed($data);
+
+        return $this;
+    }
+}
---
+++ src/ApiRuntimeInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kiboko\Component\Runtime\Api;
+
+use Kiboko\Component\Runtime\Hook\HookRuntimeInterface;
+
+interface ApiRuntimeInterface
+{
+    public function addHook(string $hookName, HookRuntimeInterface $hookRuntime): self;
+    public function feed(string $hookName, ...$data): self;
+    public function run(string $hookName): int;
+}
